e244e52542fdc3d2a583ceb3958203cfb0faaf08
Tobias Lins <tobias@lins.in>
2016-12-06 10:05:10 +0100

    Update error.php

--- error.php
+++ error.php
@@ -1,10 +1,3 @@
 <?php
-/**
- * @author              support@mpay24.com
- * @version             $Id: error.php 5477 2013-06-18 13:00:17Z anna $
- * @filesource          error.php
- * @license             http://ec.europa.eu/idabc/eupl.html EUPL, Version 1.1
- */
-
-echo "Fehler bei der Bezahlung";
+  echo "Fehler bei der Bezahlung";
 ?>

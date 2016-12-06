e55893c2734a8d3ddf1bfc55c4df0ad36de9c36f
Tobias Lins <tobias@lins.in>
2016-12-06 10:04:38 +0100

    Update pay.php

--- pay.php
+++ pay.php
@@ -8,7 +8,7 @@
   $mdxi->Order->Tid = $rand;
   
   $mdxi->Order->TemplateSet->setLanguage("DE");
-   $mdxi->Order->TemplateSet->setCSSName("MODERN");
+  $mdxi->Order->TemplateSet->setCSSName("MODERN");
   $mdxi->Order->ShoppingCart->Description = "Ihre Spende";
       
   $mdxi->Order->ShoppingCart->Item(1)->Number = 1;
@@ -26,10 +26,10 @@
   $mdxi->Order->BillingAddr->Country->setCode('AT');
   $mdxi->Order->BillingAddr->Email = $_POST['email'];
 
-    $mdxi->Order->URL->Success = substr($_SERVER['HTTP_REFERER'], 0, strrpos($_SERVER['HTTP_REFERER'], '/')) . "/success.php";
-    $mdxi->Order->URL->Error = substr($_SERVER['HTTP_REFERER'], 0, strrpos($_SERVER['HTTP_REFERER'], '/')) . "/error.php";
-    $mdxi->Order->URL->Confirmation = substr($_SERVER['HTTP_REFERER'], 0, strrpos($_SERVER['HTTP_REFERER'], '/')) . "/confirm.php?token=";
-    $mdxi->Order->URL->Cancel = substr($_SERVER['HTTP_REFERER'], 0, strrpos($_SERVER['HTTP_REFERER'], '/')) . "/index.php";
+  $mdxi->Order->URL->Success = substr($_SERVER['HTTP_REFERER'], 0, strrpos($_SERVER['HTTP_REFERER'], '/')) . "/success.php";
+  $mdxi->Order->URL->Error = substr($_SERVER['HTTP_REFERER'], 0, strrpos($_SERVER['HTTP_REFERER'], '/')) . "/error.php";
+  $mdxi->Order->URL->Confirmation = substr($_SERVER['HTTP_REFERER'], 0, strrpos($_SERVER['HTTP_REFERER'], '/')) . "/confirm.php?token=";
+  $mdxi->Order->URL->Cancel = substr($_SERVER['HTTP_REFERER'], 0, strrpos($_SERVER['HTTP_REFERER'], '/')) . "/index.php";
 
   header('Location: '.$shop->selectPayment($mdxi)->location);
 ?>
